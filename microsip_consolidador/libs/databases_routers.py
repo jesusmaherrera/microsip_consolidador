e8763651c5935d12f93a5413ea593dea16043f64
Jesús Herrera <jesusmaherrera@gmail.com>
2014-04-01 17:50:36 -0700

    para api compilado

--- microsip_consolidador/libs/databases_routers.py
+++ microsip_consolidador/libs/databases_routers.py
@@ -4,17 +4,17 @@ class MainRouter(object):
     """
     def db_for_read(self, model, **hints):
         if model._meta.app_label == 'config':
-            from microsip_api.middleware import my_local_global
+            from microsip_api.comun.middleware import my_local_global
             return '%02d-CONFIG'% my_local_global.conexion_activa
         elif model._meta.app_label == 'auth':
             return 'default'
         elif model._meta.app_label == 'django':
             return 'default'
-        elif model._meta.app_label == 'microsip_api' or model._meta.app_label == 'cuentas_por_pagar' or\
+        elif model._meta.app_label == 'models_base' or model._meta.app_label == 'cuentas_por_pagar' or\
             model._meta.app_label == 'cuentas_por_cobrar' or model._meta.app_label == 'ventas' or\
             model._meta.app_label == 'punto_de_venta' or model._meta.app_label == 'contabilidad':
 
-            from microsip_api.middleware import my_local_global
+            from microsip_api.comun.middleware import my_local_global
             if my_local_global.conexion_activa != None  and my_local_global.database_name != None:
                 return '%02d-%s'% (my_local_global.conexion_activa ,my_local_global.database_name)
 
@@ -25,16 +25,16 @@ class MainRouter(object):
         Attempts to write auth models go to auth_db.
         """
         if model._meta.app_label == 'config':
-            from microsip_api.middleware import my_local_global
+            from microsip_api.comun.middleware import my_local_global
             return '%02d-CONFIG'% my_local_global.conexion_activa
         elif model._meta.app_label == 'auth':
             return 'default'
         elif model._meta.app_label == 'django':
             return 'default'
-        elif model._meta.app_label == 'microsip_api' or model._meta.app_label == 'cuentas_por_pagar' or\
+        elif model._meta.app_label == 'models_base' or model._meta.app_label == 'cuentas_por_pagar' or\
             model._meta.app_label == 'cuentas_por_cobrar' or model._meta.app_label == 'ventas' or\
             model._meta.app_label == 'punto_de_venta' or model._meta.app_label == 'contabilidad':
-            from microsip_api.middleware import my_local_global
+            from microsip_api.comun.middleware import my_local_global
             return '%02d-%s'% (my_local_global.conexion_activa ,my_local_global.database_name)
 
         return None
@@ -47,10 +47,10 @@ class MainRouter(object):
            return True
         if (obj1._meta.app_label  == 'auth' and obj2._meta.app_label == 'django') or (obj1._meta.app_label  == 'django' and obj2._meta.app_label == 'auth'):
            return True           
-        elif (obj1._meta.app_label == 'microsip_api' or obj1._meta.app_label == 'cuentas_por_pagar' or\
+        elif (obj1._meta.app_label == 'models_base' or obj1._meta.app_label == 'cuentas_por_pagar' or\
             obj1._meta.app_label == 'cuentas_por_cobrar' or obj1._meta.app_label == 'ventas' or\
             obj1._meta.app_label == 'punto_de_venta' or model._meta.app_label == 'contabilidad') and \
-            (obj2._meta.app_label == 'microsip_api' or obj2._meta.app_label == 'cuentas_por_pagar' or\
+            (obj2._meta.app_label == 'models_base' or obj2._meta.app_label == 'cuentas_por_pagar' or\
             obj2._meta.app_label == 'cuentas_por_cobrar' or obj2._meta.app_label == 'ventas' or\
             obj2._meta.app_label == 'punto_de_venta' or model._meta.app_label == 'contabilidad'):
             return True
@@ -69,7 +69,7 @@ class MainRouter(object):
         elif model._meta.app_label == 'django' and db != 'default':
             return False
         elif db == 'default' and \
-            ( model._meta.app_label == 'microsip_api' or model._meta.app_label == 'cuentas_por_pagar' or  model._meta.app_label == 'ventas' or model._meta.app_label == 'cuentas_por_cobrar' or model._meta.app_label == 'punto_de_venta' or model._meta.app_label == 'contabilidad'):
+            ( model._meta.app_label == 'models_base' or model._meta.app_label == 'cuentas_por_pagar' or  model._meta.app_label == 'ventas' or model._meta.app_label == 'cuentas_por_cobrar' or model._meta.app_label == 'punto_de_venta' or model._meta.app_label == 'contabilidad'):
             return False
         else:
             return True
